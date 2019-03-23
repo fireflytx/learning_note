9ef3e6c5e39d76bbb542c453fbe2e082b636a787
fireflytx <13949334@qq.com>
2019-03-23 11:22:22 +0800

    add new A+B

--- python_code/a+b.py
+++ python_code/a+b.py
@@ -2,19 +2,23 @@ import random
 y = int(input("请输入随机最大数："))
 x = int(input("请输入算数题数量："))
 k = 4
+g = len(str(y))
+fa =f"{{:{g}}}"
+
 i = 0
 for i in range(1+(x//k)):
     if i < x//k :
-        z = k+1
+        z = k
 
     else :
-        z = x % k + 1
+        z = x % k
 
-    for j in range (z-1):
-        a = random.randint(10,y)
-        b = random.randint(10,y)
+    for j in range (z):
+        a = random.randint(0,y)
+        b = random.randint(0,y)
         c = random.choice(["+", "-"])
     
-        print(a,c,b,'=   ',end=' ')
+        print(fa.format(a),c,fa.format(b),'=      ',end=' ')
 
     print()
+    
